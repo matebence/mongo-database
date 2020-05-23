fa5bd01dc64936ec57818140fe5d26b87e68fb01
Bence <m.bence05@gmail.com>
2020-05-23 20:24:22 +0200

    pridanie prav pre vytvorenie db z app

--- scripts/messaging_service.js
+++ scripts/messaging_service.js
@@ -1,8 +1,19 @@
 db = db.getSiblingDB('admin');
-db.createUser(
-    {
-        user: 'msg_user',
-        pwd:  '5396ffd132678e2213ddb83c6bde40f6',
-        roles: [{role: 'userAdminAnyDatabase', db: 'admin'}],
-    }
-);
+db.createUser({
+    user: "msg_user",
+    pwd: "5396ffd132678e2213ddb83c6bde40f6",
+    roles: [{
+            role: "readWriteAnyDatabase",
+            db: "admin"
+        },
+        {
+            "role": "dbAdminAnyDatabase",
+            "db": "admin"
+        },
+        {
+            "role": "clusterAdmin",
+            "db": "admin"
+        },
+        "userAdminAnyDatabase"
+    ]
+})
